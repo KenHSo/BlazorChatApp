ad061dfc302f3ce262f73529bf4d813596057aa8
Kenneth Haagen Sørensen <khso63309@edu.ucl.dk>
2025-03-18 10:30:46 +0100

    Updated Blazor WASM to use live API

--- BlazorChatApp.Client/Program.cs
+++ BlazorChatApp.Client/Program.cs
@@ -6,6 +6,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri("https://blazor-chat-api-enh3c5gsd8b7becz.westeurope-01.azurewebsites.net")
+});
 
 await builder.Build().RunAsync();

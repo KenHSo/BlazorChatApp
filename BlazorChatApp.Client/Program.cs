b2785b5c9effd539ccff5f589d129fd1ab6edc58
Kenneth Haagen Sørensen <khso63309@edu.ucl.dk>
2025-03-18 12:39:33 +0100

    Fix to API end point

--- BlazorChatApp.Client/Program.cs
+++ BlazorChatApp.Client/Program.cs
@@ -8,7 +8,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient
 {
-    BaseAddress = new Uri("https://blazor-chat-api-enh3c5gsd8b7becz.westeurope-01.azurewebsites.net")
+    BaseAddress = new Uri("https://blazor-chat-api-enh3c5gsd8b7becz.westeurope-01.azurewebsites.net/api/")
 });
 
 await builder.Build().RunAsync();
